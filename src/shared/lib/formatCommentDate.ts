22e0c2738b2c5d1bb958f5051a42de50acd11801
xoangel <kennyrocker@yandex.ru>
2024-08-26 17:17:44 +0800

    Фикс даты на комментариях

--- src/shared/lib/formatCommentDate.ts
+++ src/shared/lib/formatCommentDate.ts
@@ -2,6 +2,5 @@ export default function formatCommentDate(date: Date): string{
     const formatter = new Intl.DateTimeFormat('ru-RU', { dateStyle: 'short' });
     const time = `${date.getHours() < 10 ? '0'+date.getHours() : date.getHours()}:${date.getMinutes() < 10 ? '0'+date.getMinutes() : date.getMinutes()}`;
     const dmy = formatter.format(date);
-
     return `${dmy} в ${time}`
 }
